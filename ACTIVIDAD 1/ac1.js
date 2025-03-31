fd586b1a93ba22b2dbf445a5175b22a100658c52
Quike <quike@quike.com>
2025-03-31 15:31:33 -0300

    cambios realizados

---
+++ ACTIVIDAD 1/ac1.js
@@ -0,0 +1,18 @@
+function eje() {
+    let arr = [];
+    arr.push("Enero");
+    arr.push("Febrero");
+    arr.push("Marzo");
+    arr.push("Abril");
+    arr.push("Mayo");
+    arr.push("Junio");
+    arr.push("Julio");
+    arr.push("Agosto");
+    arr.push("Septiembre");
+    arr.push("Octubre");
+    arr.push("Noviembre");
+    arr.push("Diciembre");
+    for (let i = 0; i < 12; i++) {
+        console.log(arr[i]);
+    }
+}
