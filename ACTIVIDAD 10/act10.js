c3ba56ad350971a5cf7bceff8e35bc121e6580e2
gabriel <152113631+Quikehc@users.noreply.github.com>
2025-04-13 23:57:40 -0300

    act10.js

--- ACTIVIDAD 10/act10.js
+++ ACTIVIDAD 10/act10.js
@@ -4,14 +4,13 @@ for (let i = 0; i < 13; i++) {
 }
 
 for (let i = 0; i < 36000; i++) {
-    var dado1 = Math.floor(Math.random() * 6) + 1; // Número entre 1 y 6
+    var dado1 = Math.floor(Math.random() * 6) + 1;
     var dado2 = Math.floor(Math.random() * 6) + 1;
     var suma = dado1 + dado2;
 
     resultados[suma]++;
 }
 
-// Mostrar resultados
 for (let i = 2; i <= 12; i++) {
     document.write(`Suma ${i}: ${resultados[i]} veces`);
 }
